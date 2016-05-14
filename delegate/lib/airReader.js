8ad20c9c20ad326cc710ecf4c0cd0083a7a3fba9
izzabudaka <izz.abudaka@gmail.com>
2016-05-14 19:15:59 +0100

    logging

--- delegate/lib/airReader.js
+++ delegate/lib/airReader.js
@@ -1,11 +1,12 @@
 var client = require("./serverMasterSender");
 var threshold = 150;
-var air = new (require("jsupm_gas").TP401)(0)
+var air = new (require("jsupm_gas").TP401)(1)
 var prev = 0,
   half = 150 / 2;
 
 setInterval(function() {
     var quality = air.getSample();
+    console.log(quality);
     if (prev <= 150 && quality > 150 && prev !== 0) { alert(quality); }
     prev = quality;
 }, 1000);
